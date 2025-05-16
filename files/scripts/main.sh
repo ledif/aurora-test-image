05ce8da9a29ed7246d5ce148a551742218489261
Adam Fidel <adam@fidel.id>
2025-05-15 23:22:32 -0500

    fix taidan

--- files/scripts/main.sh
+++ files/scripts/main.sh
@@ -2,4 +2,4 @@
 
 set -oue pipefail
 
-sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/terra.repo
+sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ublue-os:staging.repo
